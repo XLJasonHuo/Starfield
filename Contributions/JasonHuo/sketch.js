057bc2cbdc09921d65e54d8dd486c8637df26f44
Jason Huo <47305451+XLJasonHuo@users.noreply.github.com>
2021-01-05 14:10:31 -0500

    Create sketch.js

---
+++ Contributions/JasonHuo/sketch.js
@@ -0,0 +1,13 @@
+function draw() {
+  speed = map(mouseX, 0, width, 0, 30);
+  
+  background(0);
+  
+  translate(width/2, height/2);
+  
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+  
+}
